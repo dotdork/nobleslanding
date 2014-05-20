0eaac7232c124d5e4f27c7a770661ce079e51781
Bradley Johnson <dotdork@gmail.com>
2014-05-20 13:57:01 -0500

    allow simple formating for descriptions

--- app/helpers/checklist_items_helper.rb
+++ app/helpers/checklist_items_helper.rb
@@ -1,24 +1,40 @@
 module ChecklistItemsHelper
  
+#  def find_links(items)
+#  # iterate our items and see if we have any hyperlinks
+#    items.each do |item|
+#      words = item.name().split(' ')
+#      words.each_with_index do |word, i|
+#        if word.starts_with?('http')
+#          words[i] = link_to word, "#{word}", :target => "_blank"
+#        end        
+#      end
+#      item.name = words.join(' ')   
+#      
+#      words = item.description().split(' ')
+#      words.each_with_index do |word, i|
+#        if word.starts_with?('http')
+#          words[i] = link_to word, "#{word}", :target => "_blank"
+#        end        
+#      end
+#      item.description = words.join(' ')
+#    end
+#    items  
+#  end
+  
   def find_links(items)
-  # iterate our items and see if we have any hyperlinks
-    items.each do |item|
-      words = item.name().split(' ')
-      words.each_with_index do |word, i|
-        if word.starts_with?('http')
-          words[i] = link_to word, "#{word}", :target => "_blank"
-        end        
+    items.each_with_index do |item,i|
+      items[i].description = item.description.gsub( %r{http://[^\s<]+} ) do |url|
+        if url[/(?:png|jpe?g|gif|svg)$/]
+          "<img src='#{url}' />"
+        else
+          "<a href='#{url}'>#{url}</a>"
+        end   
       end
-      item.name = words.join(' ')   
-      
-      words = item.description().split(' ')
-      words.each_with_index do |word, i|
-        if word.starts_with?('http')
-          words[i] = link_to word, "#{word}", :target => "_blank"
-        end        
-      end
-      item.description = words.join(' ')
     end
-    items  
+    items
   end
+  
+  
+  
 end
