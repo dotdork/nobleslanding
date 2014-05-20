34e8ed6704e461cbd6f4d10c1b5a6480e6661262
Bradley Johnson <dotdork@gmail.com>
2014-05-20 14:30:39 -0500

    updated index for guest logs

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -2,4 +2,7 @@ Date::DATE_FORMATS[:default] =  "%B %e"
   
 Date::DATE_FORMATS[:from] =  "%b %e"
   
-Date::DATE_FORMATS[:to] =  "%b %e, %Y"
+Date::DATE_FORMATS[:to] =  "%b %e"
+Date::DATE_FORMATS[:year] =  "%Y"
+Date::DATE_FORMATS[:month] =  "%b"
+Date::DATE_FORMATS[:day] =  "%e"
